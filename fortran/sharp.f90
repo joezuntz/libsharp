91f27c54b60ed4de1758ea762b0583604b6aaa03
Martin Reinecke <martin@mpa-garching.mpg.de>
2019-12-06 21:21:20 +0100

    add copyright headers

--- fortran/sharp.f90
+++ fortran/sharp.f90
@@ -1,3 +1,27 @@
+!  This file is part of libsharp2.
+!
+!  libsharp2 is free software; you can redistribute it and/or modify
+!  it under the terms of the GNU General Public License as published by
+!  the Free Software Foundation; either version 2 of the License, or
+!  (at your option) any later version.
+!
+!  libsharp2 is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!  GNU General Public License for more details.
+!
+!  You should have received a copy of the GNU General Public License
+!  along with libsharp2; if not, write to the Free Software
+!  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+!  libsharp2 is being developed at the Max-Planck-Institut fuer Astrophysik
+
+!  \file sharp.f90
+!  Fortran wrapper module
+!
+!  Copyright (C) 2019 Max-Planck-Society
+!  \author Dag Sverre Seljebotn, Martin Reinecke
+
 module sharp
   use iso_c_binding
   implicit none
